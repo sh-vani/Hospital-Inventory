b79463f4ad046dd78b1216c959d4355dcb10dc64
Ankit Patidar <141131917+ankitpatidar1205@users.noreply.github.com>
2025-09-23 13:50:31 +0530

    ap

--- src/Api/BaseUrl.jsx
+++ src/Api/BaseUrl.jsx
@@ -1,6 +1,6 @@
 
-const BaseUrl = 'https://sow-backend-production-b2ae.up.railway.app/api';
+// const BaseUrl = 'https://sow-backend-production-b2ae.up.railway.app/api';
 
-// const BaseUrl = 'https://g5kqw2tn-5000.inc1.devtunnels.ms/api';
+const BaseUrl = 'https://ssknf82q-4000.inc1.devtunnels.ms/api';
 
 export default BaseUrl;
