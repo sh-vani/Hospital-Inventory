8620bd402a84f2b0e8875db62f5ff009df92d95b
ap8114 <amanpatidar086@gmail.com>
2025-09-25 18:50:25 +0530

    pull

--- src/Api/BaseUrl.jsx
+++ src/Api/BaseUrl.jsx
@@ -1,5 +1,5 @@
 
-const BaseUrl = 'https://ssknf82q-4000.inc1.devtunnels.ms/api';
+const BaseUrl = 'https://ssknf82q-3000.inc1.devtunnels.ms/api';
 
 // const BaseUrl = 'https://g5kqw2tn-5000.inc1.devtunnels.ms/api';
 
