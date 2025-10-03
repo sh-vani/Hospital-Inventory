0586bc4605b5de16dbe71726c9efc8c8191c343b
Ritika Pirag <120805086+veniyadav@users.noreply.github.com>
2025-10-03 19:15:57 +0530

    website

--- src/Api/BaseUrl.jsx
+++ src/Api/BaseUrl.jsx
@@ -1,9 +1,5 @@
 
 
 // const BaseUrl = 'https://hospitalinventory-backend-production.up.railway.app/api';
-<<<<<<< HEAD
-=======
-// const BaseUrl = 'https://ssknf82q-3000.inc1.devtunnels.ms/api';
->>>>>>> 64e5d7aeab3e15bbceedff376dfb8ecddcf3a686
 const BaseUrl = 'https://hrb5wx2v-3000.inc1.devtunnels.ms/api';
 export default BaseUrl;
